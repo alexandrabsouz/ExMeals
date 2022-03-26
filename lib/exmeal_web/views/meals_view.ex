a716d382e87f946844e8efe6df827d31ba6cf41b
alexandra <alexandrabsouz@gmail.com>
2022-03-26 15:47:57 -0300

    feat: create meal controller and view

---
+++ lib/exmeal_web/views/meals_view.ex
@@ -0,0 +1,20 @@
+defmodule ExmealWeb.MealsView do
+  use ExmealWeb, :view
+  alias Exmeal.Meal
+
+  def render("create.json", %{meal: %Meal{} = meal}) do
+    %{
+        meal:  %{
+          meal: %{
+            calories: meal.calories,
+            date: meal.date,
+            description: meal.description,
+            id: meal.id
+          }
+        },
+        message: "Meal created!"
+      }
+  end
+
+  def render("meal.json", %{meal: %Meal{} = meal}), do: %{meal: meal}
+end
