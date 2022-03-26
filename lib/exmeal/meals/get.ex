908feb0c2f64205aa39487bf1d291a7e4f866c6f
alexandra <alexandrabsouz@gmail.com>
2022-03-26 15:09:59 -0300

    feat: returns meal by id

---
+++ lib/exmeal/meals/get.ex
@@ -0,0 +1,14 @@
+defmodule Exmeal.Meals.Get do
+    alias Exmeal.{
+        Error,
+        Repo,
+        Meal
+    }
+
+    def by_id(uuid) do
+        case Repo.get(Meal, uuid) do
+            nil -> {:error, Error.build_user_not_found_error()}
+            meal -> {:ok, meal}
+        end
+    end
+end
