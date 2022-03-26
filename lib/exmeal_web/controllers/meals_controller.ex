7575c5a5457b6b5891a86b954d225bf2f2e433b7
alexandra <alexandrabsouz@gmail.com>
2022-03-26 15:57:22 -0300

    feat: show meal controller

--- lib/exmeal_web/controllers/meals_controller.ex
+++ lib/exmeal_web/controllers/meals_controller.ex
@@ -13,4 +13,12 @@ defmodule ExmealWeb.MealsController do
       |> render("create.json", meal: meal)
     end
   end
+
+  def show(conn, %{"id" => id} = params) do
+      with {:ok, meal} <- Exmeal.get_meal_by_id(id) do
+          conn
+          |> put_status(:ok)
+          |> render("meal.json", meal: meal)
+      end
+  end
 end
