2c8178990f6e698641812569ca4cc4bd9570c178
alexandra <alexandrabsouz@gmail.com>
2022-03-31 14:53:50 -0300

    feat: user table migration

---
+++ priv/repo/migrations/20220331174452_create_users_table.exs
@@ -0,0 +1,14 @@
+defmodule Exmeal.Repo.Migrations.CreateUsersTable do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :cpf, :string
+      add :email, :string
+      add :name, :string
+
+      timestamps()
+    end
+    create unique_index(:users, [:email])
+  end
+end
