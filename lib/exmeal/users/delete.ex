94f422535fe0fd9f39243df052491c9d597338ca
alexandra <alexandrabsouz@gmail.com>
2022-04-01 17:50:26 -0300

    feat: delete user

---
+++ lib/exmeal/users/delete.ex
@@ -0,0 +1,15 @@
+defmodule Exmeal.Users.Delete do
+    alias Exmeal.{
+      Error,
+      Repo,
+      Meal
+    }
+  
+    def call(id) do
+      case Repo.get(User, id) do
+        nil -> {:error, Error.build_user_not_found_error()}
+        user -> Repo.delete(user)
+      end
+    end
+  end
+  
