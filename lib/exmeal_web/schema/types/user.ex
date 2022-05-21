e75380db289ae675a7e75128d90f61abe3eb2463
alexandra <alexandrabsouz@gmail.com>
2022-05-21 12:04:39 -0300

    feat: input object user mutation

--- lib/exmeal_web/schema/types/user.ex
+++ lib/exmeal_web/schema/types/user.ex
@@ -8,4 +8,10 @@ defmodule ExmealWeb.Schema.Types.User do
     field :email, non_null(:string)
     field :cpf, non_null(:string)
   end
+
+  input_object :create_user_input do
+    field :name, non_null(:string), description: "name to user"
+    field :email, non_null(:string), description: "email to user"
+    field :cpf, non_null(:string), description: "cpf to user"
+  end
 end
