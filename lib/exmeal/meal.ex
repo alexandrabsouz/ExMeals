20b8b316ddf30bf4a583a5e0fb32851ba63c9148
alexandra <alexandrabsouz@gmail.com>
2022-05-21 11:29:29 -0300

    refactor: meals

--- lib/exmeal/meal.ex
+++ lib/exmeal/meal.ex
@@ -1,31 +1,28 @@
 defmodule Exmeal.Meal do
   use Ecto.Schema
-
   import Ecto.Changeset
 
-  alias Ecto.Changeset
   alias Exmeal.User
 
-
-  @required_params [:description, :date, :calories, :user_id]
-
-  @derive {Jason.Encoder, only: [:id, :description, :date, :calories, :user_id]}
+  @categories [:dessert, :food, :drink]
+  @fields [:description, :calories, :category, :user_id]
 
   schema "meals" do
+    field :calories, :float
+    field :category, Ecto.Enum, values: @categories
     field :description, :string
-    field :date, :date
-    field :calories, :integer
 
     belongs_to :user, User
 
     timestamps()
   end
 
-  def changeset(struct \\ %__MODULE__{}, params) do
-    struct
-    |> cast(params, @required_params)
-    |> validate_required(@required_params)
-    |> validate_length(:description, min: 3)
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @fields)
+    |> validate_required(@fields)
+    |> validate_number(:calories, greater_than: 0)
+    |> validate_length(:description, min: 2)
     |> foreign_key_constraint(:user_id)
   end
 end
--- lib/exmeal/meals/create.ex
+++ lib/exmeal/meals/create.ex
@@ -5,16 +5,9 @@ defmodule Exmeal.Meals.Create do
     Meal
   }
 
-  def call(params) do
+  ddef call(params) do
     params
     |> Meal.changeset()
     |> Repo.insert()
-    |> handle_result()
-  end
-
-  defp handle_result({:ok, %Meal{}} = result), do: result
-
-  defp handle_result({:error, result}) do
-    {:error, Error.build(:bad_request, result)}
   end
 end
