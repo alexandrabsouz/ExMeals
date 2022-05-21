5f709d583a94b6c752d8d6bb71e7c25fb7210166
alexandra <alexandrabsouz@gmail.com>
2022-05-21 12:08:44 -0300

    feat: create user resolver

--- lib/exmeal_web/resolver/user.ex
+++ lib/exmeal_web/resolver/user.ex
@@ -2,4 +2,6 @@ defmodule ExmealWeb.Resolver.User do
   alias Exmeal.Users
 
   def get(%{id: user_id}, _context), do: Users.Get.by_id(user_id)
+
+  def create(%{input: params}, _context), do: Users.Create.call(params)
 end
