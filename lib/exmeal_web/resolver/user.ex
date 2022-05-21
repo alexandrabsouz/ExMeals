d45d106f24bbe1b7b92c79d452350f0c57e3a091
alexandra <alexandrabsouz@gmail.com>
2022-05-21 14:06:00 -0300

    feat: add delete user

--- lib/exmeal_web/resolver/user.ex
+++ lib/exmeal_web/resolver/user.ex
@@ -2,6 +2,6 @@ defmodule ExmealWeb.Resolver.User do
   alias Exmeal.Users
 
   def get(%{id: user_id}, _context), do: Users.Get.by_id(user_id)
-
   def create(%{input: params}, _context), do: Users.Create.call(params)
+  def delete(%{id: id}, _context), do: Users.Create.call(id)
 end
