30c259af37cd1041605e0ff0abd6452ac9be1b0d
alexandra <alexandrabsouz@gmail.com>
2022-05-21 10:02:56 -0300

    feat: create seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,34 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Exmeal.{Meal, User, Repo}
+
+user1 = %{cpf: "31147677379", email: "berenice78@hotmail.com", name: "Amarelo Souza"}
+user2 = %{cpf: "78711160101", email: "evert.klocko@gmail.com", name: "Azulino Souza"}
+user3 = %{cpf: "56965545984", email: "lourdes_wintheiser18@yahoo.com", name: "Rosado Souza"}
+
+meals1 = %{description: "Batata Assada", date: "1999-02-20" , calories: 12}
+meals2 = %{description: "Banana Frita", date: "1998-02-20" , calories: 5}
+meals3 = %{description: "Mate-Cola", date: "1890-02-20" , calories: 10}
+
+
+#Inserting Users
+%User{id: user1_id} = user1 |> User.changeset() |> Repo.insert!()
+%User{id: user2_id} = user2 |> User.changeset() |> Repo.insert!()
+%User{id: user3_id} = user3 |> User.changeset() |> Repo.insert!()
+
+# inserting meals to user1
+meals1 |> Map.put(:user_id, user1_id) |> Meal.changeset() |> Repo.insert!()
+meals3 |> Map.put(:user_id, user1_id) |> Meal.changeset() |> Repo.insert!()
+
+#inserting meals to user2
+meals1 |> Map.put(:user_id, user2_id) |> Meal.changeset() |> Repo.insert!()
+meals2 |> Map.put(:user_id, user2_id) |> Meal.changeset() |> Repo.insert!()
+meals2 |> Map.put(:user_id, user2_id) |> Meal.changeset() |> Repo.insert!()
+
+#inserting meals to user 3
+meals2 |> Map.put(:user_id, user3_id) |> Meal.changeset() |> Repo.insert!()
+meals2 |> Map.put(:user_id, user3_id) |> Meal.changeset() |> Repo.insert!()
+meals3 |> Map.put(:user_id, user3_id) |> Meal.changeset() |> Repo.insert!()
+meals3 |> Map.put(:user_id, user3_id) |> Meal.changeset() |> Repo.insert!()
