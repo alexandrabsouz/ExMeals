e3f6e48ced101799c8aa0350553baa6a7ae6625c
alexandra <alexandrabsouz@gmail.com>
2022-05-21 10:23:41 -0300

    feat: user types

---
+++ lib/exmeal_web/schema/types/user.ex
@@ -0,0 +1,10 @@
+defmodule ExmealWeb.Schema.Types.User do
+    use Absinthe.Schema.Notation
+
+    object :user do
+        field :id, non_null(:id)
+        field :name, non_null(:string)
+        field :email, non_null(:string)
+        field :cpf, non_null(:string)
+    end
+end
