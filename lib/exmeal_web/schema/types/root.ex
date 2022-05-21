a19998759b64c90c756cee5fdb16a02fe52d6240
alexandra <alexandrabsouz@gmail.com>
2022-05-21 10:24:00 -0300

    feat: root user types

---
+++ lib/exmeal_web/schema/types/root.ex
@@ -0,0 +1,9 @@
+defmodule ExmealWeb.Schema.Types.Root do
+    use Absinthe.Schema.Notation
+
+    object :root_query do
+        field :user, type: :user do
+            arg :id, non_null(:id)
+        end
+    end
+end
