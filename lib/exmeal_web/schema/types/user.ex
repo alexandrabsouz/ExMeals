fc3b2ef22598770eb6cad6e991d40b8eb2b2d1ac
alexandra <alexandrabsouz@gmail.com>
2022-05-21 11:59:55 -0300

    docs: user type

--- lib/exmeal_web/schema/types/user.ex
+++ lib/exmeal_web/schema/types/user.ex
@@ -1,6 +1,7 @@
 defmodule ExmealWeb.Schema.Types.User do
   use Absinthe.Schema.Notation
 
+  @desc "Logical user representation"
   object :user do
     field :id, non_null(:id)
     field :name, non_null(:string)
